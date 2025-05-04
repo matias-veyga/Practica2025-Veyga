3c5457ad1bc371f3a89c9887055a8302ae2c575f
Matias <matuteluna114@gmail.com>
2025-05-03 23:14:01 -0300

    listo :D en el Readme le dejo un mensaje

--- GestionBanco/src/main/java/com/GestionBanco/GestionBanco/GestionBancoApplication.java
+++ GestionBanco/src/main/java/com/GestionBanco/GestionBanco/GestionBancoApplication.java
@@ -2,8 +2,10 @@ package com.GestionBanco.GestionBanco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "controller"+"entity")
 public class GestionBancoApplication {
 
 	public static void main(String[] args) {
